c00a374d297a418f5a09f3dc16e7e2625f00823e
Atsushi Sasaki <atsaki01@gmail.com>
2015-07-23 02:23:40 +0900

    add vagrant driver test for digitalocean and cloudstack provider

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -3,4 +3,6 @@ set :backend, :ssh
 set :host, ENV['KITCHEN_HOSTNAME']
 set :ssh_options, :user => ENV['KITCHEN_USERNAME'],
                   :port => ENV['KITCHEN_PORT'],
-                  :keys => ENV['KITCHEN_SSH_KEY']
+                  :keys => ENV['KITCHEN_SSH_KEY'],
+                  :paranoid => false,
+                  :user_known_hosts_file => '/dev/null'
